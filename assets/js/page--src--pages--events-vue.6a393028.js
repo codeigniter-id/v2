ff19e51c3a122feca8a9e6f0b432a036c4553bf1
puterakahfi <puterakahfi@gmail.com>
2019-06-15 15:54:06 +0700

    Updates

---
+++ assets/js/page--src--pages--events-vue.6a393028.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{397:function(t,e,n){"use strict";n.r(e);var s={},i=n(31),l=Object(i.a)(s,function(){var t=this.$createElement,e=this._self._c||t;return e("Layout",[e("div",[e("h1",{staticClass:"title is-4"},[this._v("Event")])])])},[],!1,null,null,null);e.default=l.exports}}]);
